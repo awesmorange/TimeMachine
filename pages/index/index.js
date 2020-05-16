5aead9a4594c28ac41e1cdae902f318b380f4b7a
wlan.orange <1593802539@qq.com>
2020-05-17 07:20:16 +0800

    项目搭建

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,64 +1,138 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: '欢迎进入三件事管理平台',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    lisenIt:wx.canIUse('button.open-type.getListen')
+    // 被点击的首页导航
+    currentIndexNav:0,
+    //首页导航数据
+    navList:[],
+    // 轮播图数据
+    swiperList:[],
+    // 视频列表
+    videosList:[]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+
+  /**
+   * 点击首页导航按钮
+   */
+  activeNav(e){
+    this.setData({
+      currentIndexNav:e.target.dataset.index
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
+  /**
+   * 获取首页导航数据
+   */
+  getNavList(){
+    let that = this;
+    //利用小程序内置发送请求的方法
+    wx.request({
+      url:"https://www.fastmock.site/mock/c8b94bc7b5f6687ccfe99d2c4132803f/bi/navList",
+       success(res){
+        //console.log(res);
+        if(res.data.code===0){
+          that.setData({
+            navList:res.data.data.navList
           })
         }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+     }
     })
   },
-  getListen:function(e){
-    console.log(e)
-    var v = new SpeechSynthesisUtterance();
-  	v.text = '你好';
-  	//中文
-  	v.lang = 'zh';
-  	v.rate = 0.7;
-  	speechSynthesis.speak(v);
+/**
+ * 获取轮播图数据
+ */
+getSwiperList(){
+  let that = this;
+  wx.request({
+    url: 'https://www.fastmock.site/mock/c8b94bc7b5f6687ccfe99d2c4132803f/bi/swiperList',
+    success(res){
+      if(res.data.code===0){
+        that.setData({
+          swiperList:res.data.data.swiperList
+        })
+      }
+    }
+  })
+},
+/**
+ * 获取视频列表 
+ */
+getVideosList(){
+  let that = this;
+  wx.request({
+    url: 'https://www.fastmock.site/mock/c8b94bc7b5f6687ccfe99d2c4132803f/bi/videosList',
+    success(res){
+      console.log(res.data.code)
+      if(res.data.code===0){
+        that.setData({
+          videosList:res.data.data.videosList
+        })
+      }
+      console.log(res.data.data.videosList)
+    }
+  })
+  //console.log(that.videosList)
+},
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    //1.获取首页导航数据
+    this.getNavList()
+    // 2. 获取轮播图数据
+    this.getSwiperList()
+    // 3. 获取视频列表数据
+    this.getVideosList()
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
   }
 })
--- utils/voice.js
+++
@@ -1,9 +0,0 @@
-// 文字转语音
-export function speak(text) {
-	var v = new SpeechSynthesisUtterance();
-	v.text = text;
-	//中文
-	v.lang = 'zh';
-	v.rate = 0.7;
-	speechSynthesis.speak(v);
-}
